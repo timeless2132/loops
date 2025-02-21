28ff647351216cb9f5eec0f04e1d0248f1f00186
Carter Duffy <carterduffy4@gmail.com>
2025-02-21 14:36:52 -0500

    made a loading screen method

--- loops/Program.cs
+++ loops/Program.cs
@@ -1,11 +1,61 @@
-﻿using System.Transactions;
+﻿using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
 
 namespace loops
 {
     internal class Program
     {
+       
+        public static void loading()
+        {
+            Console.Clear();
+            string loading = "Loading";
+            int loadingTime = 250;
+            loading = "Loading";
+            Console.WriteLine(loading);
+            Thread.Sleep(loadingTime);
+            loading = "Loading.";
+            Console.Clear();
+            Console.WriteLine(loading);
+            Thread.Sleep(loadingTime);
+            loading = "Loading..";
+
+            Thread.Sleep(loadingTime);
+            loading = "Loading...";
+            Console.Clear();
+            Console.WriteLine(loading);
+
+            Thread.Sleep(loadingTime);
+            loading = "Loading";
+            Console.Clear();
+            Console.WriteLine(loading);
+
+            Thread.Sleep(loadingTime);
+            loading = "Loading.";
+            Console.Clear();
+            Console.WriteLine(loading);
+
+            Thread.Sleep(loadingTime);
+            loading = "Loading..";
+            Console.Clear();
+            Console.WriteLine(loading);
+
+            Thread.Sleep(loadingTime);
+            loading = "Loading...";
+            Console.Clear();
+            Console.WriteLine(loading);
+            Thread.Sleep(loadingTime);
+            Console.Clear();
+            
+        }
+        
         static void Main(string[] args)
         {
+            
+            
+            
             bool menu = true;
 
             while (menu == true)
@@ -15,10 +65,10 @@ namespace loops
                 Console.WriteLine("Choose one on the following:");
                 Console.WriteLine();
 
-                Console.WriteLine("1 - Promter");
+                Console.WriteLine("1 - Prompter");
                 Console.WriteLine();
 
-                Console.WriteLine("2 - Menu Option 2");
+                Console.WriteLine("2 - Bank of Blorb");
                 Console.WriteLine();
 
                 Console.WriteLine("x - Menu Option X");
@@ -29,8 +79,58 @@ namespace loops
                 Console.WriteLine("Enter: ");
                 var userChoice = Console.ReadLine().ToLower().Trim();
                 Console.WriteLine();
+
+               
                 
-                if (userChoice == "Promter" || userChoice == "1")
+                if (userChoice == "bank of blorb" || userChoice == "2")
+                {
+                    double balence = 150;
+                    bool lobby = true;
+                    bool depositLoop = false;
+                    bool withdrawalLoop = false;
+                    bool billPaymentLoop = false;
+                    bool accBallenceLoop = false;
+
+
+
+                    while (lobby == true)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Welcome to The Bank of Blorb.");
+                        Console.WriteLine();
+                        Console.WriteLine("1 - Deposit");
+                        Console.WriteLine("2 - Withdrawal");
+                        Console.WriteLine("3 - Bill Payment");
+                        Console.WriteLine("4 - Account Balence");
+
+                        var lobbyChoice = Console.ReadLine().ToLower().Trim();
+                        Console.WriteLine();
+
+                        if (lobbyChoice == "1" || lobbyChoice == "deposit")
+                        {
+                            loading();
+                            Console.Clear();
+                            lobby = false;
+                            depositLoop = true;
+
+                            Console.WriteLine("Enter How Much You Would Like to Deposit:");
+                            double deposit = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine();
+
+                            balence = (deposit + balence) - 0.75;
+                            Console.WriteLine("New balence: " + balence);
+
+                        }
+
+
+                    }
+                     
+
+                }
+
+
+
+                if (userChoice == "prompter" || userChoice == "1")
                 {
                     Console.Clear();
                     menu = false;
@@ -75,8 +175,7 @@ namespace loops
                             }
                             else
                                 Console.WriteLine("Enter a Valid Number");
-                          
-
+                                Thread.Sleep(1000);
                         }
                         
                     }
@@ -85,5 +184,7 @@ namespace loops
 
             }
         }
+
+      
     }
 }
